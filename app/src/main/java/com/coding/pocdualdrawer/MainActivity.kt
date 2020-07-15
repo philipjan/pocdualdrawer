574e21ddf762a963b6a47a25e4d86ddbfa7a8b35
ph <philipjanbaruis@gmail.com>
2020-07-15 19:07:53 +0800

    base source

---
+++ app/src/main/java/com/coding/pocdualdrawer/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.coding.pocdualdrawer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.coding.pocdualdrawer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binder: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binder.leftBtn.setOnClickListener {
+            binder.drawer.openDrawer(binder.leftNav)
+        }
+        binder.rightBtn.setOnClickListener {
+            binder.drawer.openDrawer(binder.rightNav)
+        }
+    }
+}
